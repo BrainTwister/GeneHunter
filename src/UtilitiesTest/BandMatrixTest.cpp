4b160ea56a47c272b9ce93158d9912a0484eaff8
Bernd Doser <bernd.doser@braintwister.eu>
2014-10-23 21:42:25 +0200

    testing BandMatrix

--- src/UtilitiesTest/BandMatrixTest.cpp
+++ src/UtilitiesTest/BandMatrixTest.cpp
@@ -3,56 +3,45 @@
 
 using namespace std;
 
-TEST(BandMatrixTest,TestAccess)
+TEST(BandMatrixTest,Case0)
 {
 	BandMatrix<double> A(3,3,1,1);
+	A.setToZero();
 	A[2][1] = 7.0;
 
-	EXPECT_EQ(7.0,A[2][1]);
-	EXPECT_THROW(A[2][0],std::exception);
+	EXPECT_EQ(7.0, A[2][1]);
+	EXPECT_THROW(A[2][0], std::exception);
 }
 
-#if 0
+TEST(BandMatrixTest,Case1)
 {
 	BandMatrix<double> A(5,5,1,1);
 	A.setToZero();
-
 	A[2][1] = 2.0;
 
-	if ( A[2][1] != 2.0 ) testSuite.errorLog(name() + ": wrong element 2/1");
-
-	try {
-		A[2][0] = 3.0;
-		testSuite.errorLog(name() + ": forbidden write access 2/1");
-	} catch ( std::exception const& e ) {}
-
-	try {
-		A[0][2] = 3.0;
-		testSuite.errorLog(name() + ": forbidden write access 2/2");
-	} catch ( std::exception const& e ) {}
+	EXPECT_DOUBLE_EQ(2.0, A[2][1]);
+	EXPECT_THROW(A[2][0], std::exception);
+	EXPECT_THROW(A[0][2], std::exception);
 }
+
+TEST(BandMatrixTest,Case2)
 {
 	BandMatrix<double> A(3,5,1,1);
 	A.setToZero();
-
 	A[2][1] = 2.0;
 
-	//cout << A << endl;
-
-	if ( A[2][1] != 2.0 ) testSuite.errorLog(name() + ": wrong element 2/1");
-
-	try {
-		A[2][0] = 3.0;
-		testSuite.errorLog(name() + ": forbidden write access 2/1");
-	} catch ( std::exception const& e ) {}
-
-	try {
-		A[0][2] = 3.0;
-		testSuite.errorLog(name() + ": forbidden write access 2/2");
-	} catch ( std::exception const& e ) {}
+	EXPECT_DOUBLE_EQ(2.0, A[2][1]);
+	EXPECT_THROW(A[2][0], std::exception);
+	EXPECT_THROW(A[0][2], std::exception);
 }
+
+TEST(BandMatrixTest,Case3)
 {
 	BandMatrix<double> A(10,7,2,2);
 	A.setToZero();
+	A[2][1] = 2.0;
+
+	EXPECT_DOUBLE_EQ(2.0, A[2][1]);
+	EXPECT_THROW(A[3][0], std::exception);
+	EXPECT_THROW(A[0][3], std::exception);
 }
-#endif
